02f7d0a83b9523de09eab9f276c4f2954d6def05
Akshita Srivastava <akshita.srivastava.1505@gmail.com>
2024-08-27 23:26:31 +0530

    arr

---
+++ array/basic.java
@@ -0,0 +1,17 @@
+package array;
+//collection of similar dATA type
+//contiguous memory allocastion
+
+
+public class basic {
+    public static void main(String[] args) {
+        // array declaration
+        
+        int ages[];
+        ages=new int[10];
+        int weight[]=new int[10];
+        
+
+    }
+    
+}

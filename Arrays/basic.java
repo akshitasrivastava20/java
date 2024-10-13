631c0c0d6041265993ab78f4be7d5f46728f67ef
Akshita Srivastava <akshita.srivastava.1505@gmail.com>
2024-10-13 22:44:07 +0530

    idk

---
+++ Arrays/basic.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class basic {
+    public static void main(String[] args) {
+        //syntax
+        //dtatatyper[] variable name=new datatype[size];
+        int[] arr=new int[5];
+        int[] arr1={1,2,3,4,5};  
+        System.out.println(arr[1]);
+        String[] str=new String[5];
+        System.out.println(str[1]); 
+
+    }
+}
